b10f26eab94a06639fe490d9d0431309af5ef3ff
John Millikin <jmillikin@gmail.com>
2008-03-15 12:28:22 -0700

    Use jsonlib.tests.suite() to determine which tests to execute.

--- tests.py
+++ tests.py
@@ -2,16 +2,5 @@
 # Author: John Millikin <jmillikin@gmail.com>
 
 import unittest
-from jsonlib import read_tests, write_tests
-
-def main ():
-	from_module = unittest.TestLoader ().loadTestsFromModule
-	all_tests = unittest.TestSuite ()
-	all_tests.addTests (from_module (read_tests))
-	all_tests.addTests (from_module (write_tests))
-	unittest.TextTestRunner ().run (all_tests)
-	
-
-if __name__ == '__main__':
-	main ()
-	
+from jsonlib import tests
+unittest.TextTestRunner ().run (tests.suite ())
